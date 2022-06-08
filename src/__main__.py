d8ca06d95c3f864e0fdb0338157d632c6ee2e092
Advik <advik.b@gmail.com>
2022-06-08 15:21:40 +0530

    If you are seeing this. This commit has gained the approval of @Griphcode, @Oxmc, @givinghawk
    Co-Authored-By: Griphcode <griphcode@protonmail.com>
    Co-Authored-By: oxmc <67136658+oxmc@users.noreply.github.com>
    Co-Authored-By: Givinghawk <gh@givinghawk.xyz>

--- src/__main__.py
+++ src/__main__.py
@@ -250,7 +250,38 @@ class Editor(QMainWindow):
         QMessageBox.aboutQt(self, "About Qt")
 
     def license_(self):
-        print("License")
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("License Information")
+        msg.setText('The license is for the "Griph-Pad" application. Not Qt.')
+        msg.setInformativeText(
+            
+            "MIT License\n"
+            "\n"
+            "Copyright (c) 2022-Present Griphitor Team <https://github.com/Griphitor>\n"
+            "\n"
+            "Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+            "of this software and associated documentation files (the 'Software'), to deal\n"
+            "in the Software without restriction, including without limitation the rights\n"
+            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+            "copies of the Software, and to permit persons to whom the Software is\n"
+            "furnished to do so, subject to the following conditions:\n"
+            "\n"
+            "The above copyright notice and this permission notice shall be included in all\n"
+            "copies or substantial portions of the Software.\n"
+            "\n"
+            "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+            "SOFTWARE."
+        )
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.setWindowIcon(self.windowIcon())
+        msg.exec_()
+        
 
     def credits_(self):
         print("Credits")
